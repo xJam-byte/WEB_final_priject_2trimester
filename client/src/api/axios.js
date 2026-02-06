f758c80d103233938bf0db5e92f5cdd98a1e1664
xJam-byte <abylaimuratbayev@gmail.com>
2026-02-06 12:42:18 +0500

    client

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "/api",
+  baseURL: "https://web-final-priject-2trimester-1.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
